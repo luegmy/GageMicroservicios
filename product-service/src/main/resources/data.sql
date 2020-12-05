eb7d12addd3c10f6ddd4dd3153bb616a74fb6ed1
jneyra <luegmy@gmail.com>
2020-12-04 23:59:56 -0500

    pruebas unitarias y reporte con jacoco, falta hacer test metodo eliminar

--- product-service/src/main/resources/data.sql
+++ product-service/src/main/resources/data.sql
@@ -1,4 +1,5 @@
-INSERT INTO tb_tipoproducto(cod_tipo,descripcion) VALUES (1,'herramientas');
+INSERT INTO tb_tipoproducto(cod_tipo,descripcion) VALUES (1,'alumbrado');
+INSERT INTO tb_tipoproducto(cod_tipo,descripcion) VALUES (2,'herramientas');
 INSERT INTO tb_medida (cod_medida,descripcion,abreviatura) VALUES(58,'Unidad','NIU');
 
 INSERT INTO tb_producto(cod_producto,descripcion,precio_compra,precio_venta,cod_tipo,cod_medida,fecha,existencia)
--- product-service/src/test/java/com/gage/store/product/ProductServiceMockTest.java
+++ product-service/src/test/java/com/gage/store/product/ProductServiceMockTest.java
@@ -1,14 +1,15 @@
 package com.gage.store.product;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.gage.store.product.entity.Categoria;
@@ -20,7 +21,7 @@ import com.gage.store.product.service.ProductoServicioImp;
 @SpringBootTest
 public class ProductServiceMockTest {
 
-	ProductoRepositorio repositorioMock=Mockito.mock(ProductoRepositorio.class);
+	ProductoRepositorio repositorioMock = Mockito.mock(ProductoRepositorio.class);
 
 	ProductoServicio servicio;
 
@@ -31,16 +32,24 @@ public class ProductServiceMockTest {
 
 		Producto prod = Producto.builder().categoria(Categoria.builder().codTipo(1).build()).descripcion("linterna")
 				.precioCompra(new BigDecimal(5.5)).precioVenta(new BigDecimal(10)).existencia(100).build();
+		Producto prod2 = Producto.builder().categoria(Categoria.builder().codTipo(2).build()).descripcion("martillo")
+				.precioCompra(new BigDecimal(7.5)).precioVenta(new BigDecimal(14)).existencia(10).build();
+
+		List<Producto> listaProductos = Arrays.asList(prod, prod2);
 
 		Mockito.when(repositorioMock.findById(1)).thenReturn(Optional.of(prod));
+		Mockito.when(repositorioMock.findById(2)).thenReturn(Optional.of(prod2));
 		Mockito.when(repositorioMock.save(prod)).thenReturn(prod);
+		Mockito.when(repositorioMock.save(prod2)).thenReturn(prod2);
+		Mockito.when(repositorioMock.findAll()).thenReturn(listaProductos);
+		Mockito.when(repositorioMock.findByCategoria(prod.getCategoria())).thenReturn(Arrays.asList(prod));
+		Mockito.when(repositorioMock.findByCategoria(prod2.getCategoria())).thenReturn(Arrays.asList(prod2));
 	}
 
 	@Test
 	public void whenValidGetIdThenReturnProduct() {
-		Producto prod = servicio.obtenerProducto(1);
-		Assertions.assertThat(prod.getDescripcion()).isEqualTo("linterna");
-
+		Producto prod = servicio.obtenerProducto(2);
+		Assertions.assertThat(prod.getDescripcion()).isEqualTo("martillo");
 	}
 
 	@Test
@@ -49,6 +58,18 @@ public class ProductServiceMockTest {
 		newStock.setExistencia(newStock.getExistencia() + 5);
 		servicio.guardarProducto(newStock);
 		Assertions.assertThat(newStock.getExistencia()).isEqualTo(105);
+	}
+
+	@Test
+	public void whenFindAllProductThenRetunListProducto() {
+		List<Producto> listaProducto = servicio.listarProducto();
+		Assertions.assertThat(listaProducto.size()).isEqualTo(2);
+	}
+
+	@Test
+	public void whenFindByCategoryThenRetunListProducto() {
+		List<Producto> listaProducto = servicio.listarProductoPorCategoria(Categoria.builder().codTipo(2).build());
+		Assertions.assertThat(listaProducto.size()).isEqualTo(1);
 	}
 
 }
